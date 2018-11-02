6076ecbd42155fad867529b194ec55ce0a158918
Daniel Westphal <westphaldp@gmail.com>
2018-11-02 02:24:52 +0000

    Added license reference to header.

--- avvem.py
+++ avvem.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python2
 
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
 import os, sys
 import subprocess
 from time import sleep
